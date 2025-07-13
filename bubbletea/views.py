36a1a42ecf65c41448d68254225a43370215bd72
Bishwas <thapabishwas123@gmail.com>
2025-07-13 13:52:09 +0545

    Initial commit on test branch

--- bubbletea/views.py
+++ bubbletea/views.py
@@ -115,3 +115,11 @@ def logout_view(request):
 
     # Redirect to login or home page
     return redirect('home')  # or redirect('home')
+def Admin_Panel(request):
+    context = {
+        'total_sales': Order.objects.count(),
+        'total_users': User.objects.exclude(role='admin').count(),
+        'total_products': Product.objects.count(),
+        'today_orders': Order.objects.filter(orderDate=timezone.now().date()).count(),
+    }
+    return render(request, 'adminPanel.html', context)
